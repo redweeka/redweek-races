b57213d809502ef1759e40fb53744bd5a7498526
redweeka <redweek2000@gmail.com>
2021-10-29 01:04:46 +0300

    move from java to java in android

---
+++ RedweekRaces/app/src/main/java/com/example/redweekraces/services/RaceFactory.java
@@ -0,0 +1,116 @@
+package com.example.redweekraces.services;
+
+import com.example.redweekraces.models.Rider;
+
+import java.util.*;
+
+public class RaceFactory {
+    private final int[] TRACKS = {100, 200, 300};
+    private final int RIDERS_AMOUNT = 99;
+    private final int RACE_RIDERS_AMOUNT = 5;
+    private final Random randomGenerator = new Random();
+    private final List<Rider> riders;
+
+    // Integer for easy sorting
+    private Integer[] raceRidersIndexes;
+    private static final RaceFactory instance = new RaceFactory();
+
+    public static RaceFactory getInstance(){
+        return instance;
+    }
+
+    private RaceFactory() {
+        this.riders = new ArrayList<>();
+
+        for (int i = 1; i <= this.RIDERS_AMOUNT; i++) {
+            this.riders.add(new Rider(i + ""));
+        }
+        //System.out.println(Arrays.deepToString(this.riders.toArray()));
+
+        this.raceRidersIndexes = new Integer[this.RACE_RIDERS_AMOUNT];
+    }
+
+    public String race() {
+        chooseRidersForRace();
+
+        // Default race track
+        Integer[] ridersIndexesRaceResult = startRace(0);
+        // Called before endRace() to use steps info before reset
+        terminalDisplayRaceResult(ridersIndexesRaceResult);
+        String winnerName = this.riders.get(ridersIndexesRaceResult[0]).name();
+        endRace();
+
+        return winnerName;
+    }
+
+    private void chooseRidersForRace() {
+        boolean[] numbers = new boolean[this.RIDERS_AMOUNT];
+
+        // Choose random race riders
+        for (int raceRidersIndex = 0; raceRidersIndex < this.RACE_RIDERS_AMOUNT; raceRidersIndex++) {
+            int riderIndex = this.randomGenerator.nextInt(this.riders.size());
+
+            if(numbers[riderIndex]){
+                raceRidersIndex--;
+            } else {
+                numbers[riderIndex] = true;
+                this.raceRidersIndexes[raceRidersIndex] = riderIndex;
+            }
+        }
+    }
+
+    private Integer[] startRace(int trackIndex) {
+        final int trackLength = this.TRACKS[trackIndex];
+        final int ESTIMATED_TURNS = 10;
+        final int DISTANCE_FACTOR = trackLength / ESTIMATED_TURNS;
+        boolean raceOn = true;
+        Rider currRider;
+
+        while (raceOn) {
+            for (int raceRidersIndex = 0; raceRidersIndex < this.RACE_RIDERS_AMOUNT; raceRidersIndex++) {
+                currRider = this.riders.get(this.raceRidersIndexes[raceRidersIndex]);
+                int addDistanceFactor = DISTANCE_FACTOR + currRider.stepsBonusChances();
+                int trackDistance = this.randomGenerator.nextInt(addDistanceFactor) + 1;
+                currRider.raiseDistanceInRace(trackDistance);
+
+                if (currRider.distanceInRace() >= trackLength) {
+                    raceOn = false;
+                    break;
+                }
+            }
+
+            Comparator<Integer> stepsCmp =  Comparator.comparing(nextRider -> riders.get(nextRider).distanceInRace());
+            Arrays.sort(this.raceRidersIndexes, stepsCmp.reversed());
+        }
+
+        return this.raceRidersIndexes;
+    }
+
+    private void endRace() {
+        //TODO: 10/24/2021 take care of winner and losers stats by making func for it in Rider class
+        Rider currRider = this.riders.get(this.raceRidersIndexes[0]);
+        currRider.resetDistanceInRace();
+
+        // For the losers
+        for (int raceRidersIndex = 1; raceRidersIndex < this.RACE_RIDERS_AMOUNT; raceRidersIndex++) {
+            currRider = this.riders.get(this.raceRidersIndexes[raceRidersIndex]);
+            currRider.resetDistanceInRace();
+        }
+
+        this.raceRidersIndexes = new Integer[this.RACE_RIDERS_AMOUNT];
+    }
+
+    private void terminalDisplayRaceResult(Integer[] ridersIndexesRaceResult) {
+        Rider currRider;
+
+        for (int raceRidersIndex = 0; raceRidersIndex < ridersIndexesRaceResult.length; raceRidersIndex++) {
+            currRider = this.riders.get(ridersIndexesRaceResult[raceRidersIndex]);
+            System.out.println("Place no. "
+                    + (raceRidersIndex + 1)
+                    + " =>  "
+                    + currRider.name()
+                    + " ||| With distance of => "
+                    + currRider.distanceInRace());
+        }
+    }
+}
---
+++ RedweekRaces/app/src/main/java/com/example/redweekraces/views/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.redweekraces.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.redweekraces.R;
+import com.example.redweekraces.services.RaceFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView raceResult;
+    Button play;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        raceResult = findViewById(R.id.race_result);
+        play = findViewById(R.id.play);
+
+        setPlayOnClickListener();
+    }
+
+    private void setPlayOnClickListener() {
+        play.setOnClickListener(v -> {
+            String winnerName = RaceFactory.getInstance().race();
+            raceResult.setText(winnerName);
+        });
+    }
+}
